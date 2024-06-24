748b63d469317a2eb3e40409a7aaca040266ce4d
Arjun P <ap2045202@gmail.com>
2024-06-24 03:24:32 -0400

    Initial Commit

---
+++ WebShield/WebShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebShieldApp.swift
+//  WebShield
+//
+//  Created by Arjun on 24/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WebShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
