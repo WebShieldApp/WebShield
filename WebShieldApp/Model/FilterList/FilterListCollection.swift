d965ace74a552355e119d4a73f6f206226eaa352
Arjun P <ap2045202@gmail.com>
2024-10-28 23:20:47 -0400

    Continuing refactor; modified gitignore

--- WebShieldApp/Model/FilterList/FilterListCollection.swift
+++ WebShieldApp/Model/FilterList/FilterListCollection.swift
@@ -6,11 +6,35 @@
 //
 
 struct FilterListCollection {
-    typealias DictionaryType = [FilterList]
+    typealias SetType = [FilterList]
 
-    private var filterLists = DictionaryType()
+    private var filterLists = SetType()
 
-    init(filterLists: DictionaryType) {
+    init(filterLists: SetType) {
         self.filterLists = filterLists
     }
+}
+
+extension FilterListCollection: Collection {
+    typealias Index = SetType.Index
+    typealias Element = SetType.Element
+
+    var startIndex: Index {
+        return filterLists.startIndex
+    }
+
+    var endIndex: Index {
+        return filterLists.endIndex
+    }
+
+    // Required subscript, based on a dictionary index
+    subscript(index: Index) -> Iterator.Element {
+        get { return filterLists[index] }
+    }
+
+    // Method that returns the next index when iterating
+    func index(after i: Index) -> Index {
+        return filterLists.index(after: i)
+    }
+
 }
--- WebShieldApp/Model/FilterList/FilterListConductor.swift
+++ WebShieldApp/Model/FilterList/FilterListConductor.swift
@@ -6,5 +6,5 @@
 //
 
 actor FilterListConductor {
-    
+
 }
